e5937d286aaa17396e6461522c150666bc7ea84d
Kim Maida <git@kim-maida.com>
2014-06-29 11:11:19 -0400

    Spaces to tabs, add classes to index

--- app/directives/ContentItemDir.js
+++ app/directives/ContentItemDir.js
@@ -1,40 +1,41 @@
 app.directive('contentItem', function ($compile) {
-    var imageTemplate = '<section image-template></section>';
-    var videoTemplate = '<section video-template></section>';
-    var noteTemplate = '<section ng-include="\'view/tpl/NoteTpl.html\'"></section>';
+	var imageTemplate = '<section image-template></section>',
+		videoTemplate = '<section video-template></section>',
+		noteTemplate = '<section ng-include="\'view/tpl/NoteTpl.html\'"></section>';
 
-    var getTemplate = function(contentType) {
-        var template = '';
+	var getTemplate = function(contentType) {
+		var template = '';
 
-        switch(contentType) {
-            case 'image':
-                template = imageTemplate;
-                break;
-            case 'video':
-                template = videoTemplate;
-                break;
-            case 'notes':
-                template = noteTemplate;
-                break;
-        }
+		switch(contentType) {
+			case 'image':
+				template = imageTemplate;
+				break;
+			case 'video':
+				template = videoTemplate;
+				break;
+			case 'notes':
+				template = noteTemplate;
+				break;
+		}
 
-        return template;
-    }
+		return template;
+	}
 
-    var linker = function(scope, element, attrs) {
-        scope.rootDirectory = 'assets/img/';
+	var linker = function(scope, element, attrs) {
+		// move this into image controller for proper ng-src
+		scope.rootDirectory = 'assets/img/';
 
-        element.html(getTemplate(scope.content.content_type)).show();
+		element.html(getTemplate(scope.content.content_type)).show();
 
-        $compile(element.contents())(scope);
-    }
+		$compile(element.contents())(scope);
+	}
 
-    return {
-        restrict: "A",
-        replace: true,
-        link: linker,
-        scope: {
-            content:'='
-        }
-    };
+	return {
+		restrict: "A",
+		replace: true,
+		link: linker,
+		scope: {
+			content:'='
+		}
+	};
 });
