acfc54864c398bad691f0ed76f1c038d15d767ac
Kim Maida <git@kim-maida.com>
2014-06-29 10:47:47 -0400

    Create tpl files for content type templates

--- app/directives/ContentItemDir.js
+++ app/directives/ContentItemDir.js
@@ -1,7 +1,7 @@
 app.directive('contentItem', function ($compile) {
     var imageTemplate = '<section image-template></section>';
-    var videoTemplate = '<div class="entry-video"><h2>&nbsp;</h2><div class="entry-vid"><iframe ng-src="{{content.data}}" width="280" height="200" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><div class="entry-text"><div class="entry-title">{{content.title}}</div><div class="entry-copy">{{content.description}}</div></div></div>';
-    var noteTemplate = '<div class="entry-note"><h2>&nbsp;</h2><div class="entry-text"><div class="entry-title">{{content.title}}</div><div class="entry-copy">{{content.data}}</div></div></div>';
+    var videoTemplate = '<section ng-include="\'view/tpl/VideoTpl.html\'"></section>';
+    var noteTemplate = '<section ng-include="\'view/tpl/NoteTpl.html\'"></section>';
 
     var getTemplate = function(contentType) {
         var template = '';
